4289f2c854761aa67916c1b59f1afb72f98ae1fb
Joseph Palacios <josephdavidpalacios@gmail.com>
2023-11-30 09:19:28 -0400

    Inputs changes

--- src/app/construccion/components/ficha-detail/ficha-detail.component.ts
+++ src/app/construccion/components/ficha-detail/ficha-detail.component.ts
@@ -33,6 +33,14 @@ export class FichaDetailComponent implements OnInit {
     'Nivel III. Desempeño de transferencia.',
     'Nivel IV. Desempeño intuitivo.'
   ];
+  horasFormacion = [
+    40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110,
+    115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175,
+    180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240
+  ];
+  areaDeConocimiento = {};
+  areaOcupacional = {};
+  subAreaOcupacional = {};
   public fichaDetailForm: FormGroup;
   public mapForm: FormGroup;
   public resourcesAndSpacesForm: FormGroup;
@@ -64,8 +72,24 @@ export class FichaDetailComponent implements OnInit {
         }
       }
     });
+    this.areaDeConocimiento = this.construccionService.areaDeConocimiento;
+    this.areaOcupacional = this.construccionService.areaOcupacional;
+    this.subAreaOcupacional = this.construccionService.subAreaOcupacional;
     this.setupForm();
   }
+  areasConocimiento(){
+    console.log(Object.entries(this.areaDeConocimiento));
+    return Object.entries(this.areaDeConocimiento);
+  }
+
+  subAreasOcupacionales(){
+    return Object.entries(this.subAreaOcupacional);
+  }
+
+  areasOcupacionales(){
+    return Object.entries(this.areaOcupacional);
+  }
+
   setupForm() {
     this.fichaDetailForm = this.formBuilder.group({
       // areaOcupacional: ['Ciencias Físicas'],
